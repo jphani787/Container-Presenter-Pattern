622ca81a52aa361f04a38bc3ca6ce6323aba31cd
jphani787 <jphanindra787@gmail.com>
2026-06-19 09:59:00 +0530

    controlled uncontrolled pattern code set up

---
+++ controlled-uncontrolled-pattern/src/App.jsx
@@ -0,0 +1,8 @@
+import "./App.css";
+//import UserProfileContainer from "./with-pattern/components/UserProfileContainer";
+
+function App() {
+  return <div>{/* <UserProfileContainer userId={1} /> */}</div>;
+}
+
+export default App;
